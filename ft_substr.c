10dc74260d2852cc6404386cf3c4efac8eba6d0e
Bas Uitermark <88148607+buiterma@users.noreply.github.com>
2021-10-26 21:55:57 +0200

    change in substring

--- ft_substr.c
+++ ft_substr.c
@@ -4,15 +4,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
+	size_t	j;
 
-	i = ft_strlen(s);
-	if (start > ft_strlen(s))
+	i = start;
+	j = 0;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
-	if (len > i)
-		return (ft_strdup(s));
 	sub = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!sub)
 		return (NULL);
-	ft_strlcpy(sub, s + start, len + 1);
+	while (j < len && s[i])
+	{
+		sub[j] = s[i]
+		j++;
+		i++;
+	}
+	sub[j] = 0;
 	return (sub);
 }
