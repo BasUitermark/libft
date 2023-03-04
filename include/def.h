3b12bf4c317e813cc82c53f5aa37961ec5d46966
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2023-03-04 09:03:00 +0100

    Changed line in def.h line 72

--- include/def.h
+++ include/def.h
@@ -6,7 +6,7 @@
 /*   By: buiterma <buiterma@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/03 15:25:47 by buiterma      #+#    #+#                 */
-/*   Updated: 2022/09/09 19:03:27 by buiterma      ########   odam.nl         */
+/*   Updated: 2023/03/04 09:03:00 by buiterma      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,6 @@ typedef struct s_data_store
 //=== Print Colors ==//
 
 # define RED		"\x1b[31m"
-<<<<<<< HEAD
 # define GREEN		"\x1b[32m"
 # define YELLOW		"\x1b[33m"
 # define BLUE		"\x1b[34m"
@@ -82,19 +81,5 @@ typedef struct s_data_store
 # define LINE		"\x1b[4m"
 # define BLINK		"\x1b[5m"
 # define CROSS		"\x1b[9m"
-=======
-# define GREEN	"\x1b[32m"
-# define YELLOW	"\x1b[33m"
-# define BLUE	"\x1b[34m"
-# define MAGENTA	"\x1b[35m"
-# define CYAN	"\x1b[36m"
-# define RESET	"\x1b[0m"
-
-# define BOLD	"\x1b[1m"
-# define ITALIC	"\x1b[3m"
-# define LINE	"\x1b[4m"
-# define BLINK	"\x1b[5m"
-# define CROSS	"\x1b[9m"
->>>>>>> refs/remotes/origin/master
 
 #endif
